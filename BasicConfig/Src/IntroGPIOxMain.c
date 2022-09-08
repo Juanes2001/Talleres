c57b6a78ba96efb098592344bf32441c0ecd9666
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2022-09-08 11:58:15 -0500

    Update IntroGPIOxMain.c

--- BasicConfig/Src/IntroGPIOxMain.c
+++ BasicConfig/Src/IntroGPIOxMain.c
@@ -14,16 +14,10 @@
 
 /*Funcion principal del programa. Es aca donde se ejecuta todo
  */
-BasicTimer_Handler_t handlerTimer2 ={0};
 
 
 int main(void){
 
-	handlerTimer2.ptrTIMx                            =TIM2;
-	handlerTimer2.TIMx_Config.TIMx_mode              = BTIMER_MODE_UP;
-	handlerTimer2.TIMx_Config.TIMx_speed             = BTIMER_SPEED_100us;
-	handlerTimer2.TIMx_Config.TIMx_interruptEnable   ;
-	handlerTimer2.TIMx_Config.TIMx_period            ;
 
 	//Este es el ciclo principal, donde se ejecuta todo el programa
 	while(1){
